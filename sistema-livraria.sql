b35f581ce873dd91a841da6eb275c8d8a49ddf46
gabrielbordignon <gabriel.bordignon28@escola.pr.gov.br>
2026-09-01 08:20:58 -0300

    Create Clientes and Compras tables in SQL

---
+++ sistema-livraria.sql
@@ -0,0 +1,14 @@
+-- Criação da Tabela Clientes
+CREATE TABLE Clientes (
+    ID INTEGER PRIMARY KEY,
+    nomeCliente VARCHAR(255),
+    emailCliente VARCHAR(255)
+);
+
+-- Criação da Tabela Compras
+CREATE TABLE Compras (
+    CompraID INTEGER PRIMARY KEY,
+    ClienteID INTEGER,
+    NomeLivro VARCHAR(255),
+    FOREIGN KEY (ClienteID) REFERENCES Clientes(ID)
+);
